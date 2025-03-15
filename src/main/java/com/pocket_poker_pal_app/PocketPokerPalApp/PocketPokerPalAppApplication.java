c893da5baddda8c2eabe5628fb0d5ca0eb41420a
Efrain Palencia <epalen6@wgu.edu>
2025-03-15 13:23:10 -0400

    Build initial spring project

---
+++ src/main/java/com/pocket_poker_pal_app/PocketPokerPalApp/PocketPokerPalAppApplication.java
@@ -0,0 +1,13 @@
+package com.pocket_poker_pal_app.PocketPokerPalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketPokerPalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketPokerPalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pocket_poker_pal_app/PocketPokerPalApp/PocketPokerPalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocket_poker_pal_app.PocketPokerPalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketPokerPalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
